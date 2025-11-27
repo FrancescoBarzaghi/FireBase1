5606733736e5a212379df0d78119746eb41cbe28
Barza <159137384+FrancescoBarzaghi@users.noreply.github.com>
2025-11-27 21:06:11 +0100

    Delete Firebase configuration from enviroment.ts
    Removed Firebase configuration from environment file.

--- AppFire/src/app/enviroment.ts
+++ AppFire/src/app/enviroment.ts
@@ -1,11 +1,1 @@
-export const enviroment = {
-  firebase: {
-    apiKey: "AIzaSyB9cw9w7skdbp4QuXwon4X_luVVAR7-EJs",
-    authDomain: "fire1-43b05.firebaseapp.com",
-    projectId: "fire1-43b05",
-    storageBucket: "fire1-43b05.appspot.com",
-    messagingSenderId: "418619911368",
-    appId: "1:418619911368:web:9a1c6b798b600d5e22c9fd",
-    measurementId: "G-ETZ1SK946S"
-  }
-};
+
